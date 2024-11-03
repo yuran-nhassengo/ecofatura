91f064631d8da31974852803290e7a981f8907fc
yuran-nhassengo <yurannhassengo@gmail.com>
2024-11-03 17:05:44 +0200

    add types

---
+++ app/types/Factura.ts
@@ -0,0 +1,10 @@
+export interface Factura {
+    codigo: string;
+    nome: string;
+    tipo: string;
+    data: string;
+    entidade: string;
+    valor: string;
+    descricao: string;
+    nuit: number | "";
+  }
