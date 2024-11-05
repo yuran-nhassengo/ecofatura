f45bc5c150364426e66330be647e7d1ad54c7a57
yuran-nhassengo <yurannhassengo@gmail.com>
2024-11-06 01:56:41 +0200

    update routes

--- app/api/facturas/[id]/route.ts
+++ app/api/facturas/[id]/route.ts
@@ -1,31 +1,34 @@
 import { deleteFactura, getFacturaById, updateFactura } from "@/app/services/factura";
 import { NextRequest, NextResponse } from "next/server";
 
-
-export async function PUT(request: NextRequest, { params }: { params: { id: string } }) {
+// PUT - Atualizar fatura
+export async function PUT(request: NextRequest, context: { params: { id: string } }) {
+    const { id } = context.params; // Acessa o id corretamente
     const body = await request.json();
     try {
-        const updatedFactura = await updateFactura(params.id, body); 
+        const updatedFactura = await updateFactura(id, body);
         return NextResponse.json(updatedFactura);
     } catch (error) {
         return NextResponse.json({ error: `Failed to update invoice: ${error}` }, { status: 500 });
     }
 }
 
-
-export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
+// DELETE - Deletar fatura
+export async function DELETE(request: NextRequest, context: { params: { id: string } }) {
+    const { id } = context.params; // Acessa o id corretamente
     try {
-        const deletedFactura = await deleteFactura(params.id);
+        const deletedFactura = await deleteFactura(id);
         return NextResponse.json(deletedFactura);
     } catch (error) {
         return NextResponse.json({ error: `Error deleting the item: ${error}` }, { status: 500 });
     }
 }
 
-
-export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
+// GET - Obter fatura
+export async function GET(request: NextRequest, context: { params: { id: string } }) {
+    const { id } = context.params; // Acessa o id corretamente
     try {
-        const factura = await getFacturaById(params.id); 
+        const factura = await getFacturaById(id);
 
         if (!factura) {
             return NextResponse.json({ error: `Factura not found` }, { status: 404 });
