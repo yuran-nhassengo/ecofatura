34f6fb817bae7cd6b51191ea88ae05ffe8f67265
yuran-nhassengo <yurannhassengo@gmail.com>
2024-11-06 01:40:01 +0200

    add nextRequest

--- app/api/facturas/[id]/route.ts
+++ app/api/facturas/[id]/route.ts
@@ -1,36 +1,39 @@
 import { deleteFactura, getFacturaById, updateFactura } from "@/app/services/factura";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function PUT(request: Request, { params }: { params: { id: string } }) {
+
+export async function PUT(request: NextRequest, { params }: { params: { id: string } }) {
     const body = await request.json();
     try {
-        const updatedFactura = await updateFactura((params.id), body);
+        const updatedFactura = await updateFactura(params.id, body); 
         return NextResponse.json(updatedFactura);
     } catch (error) {
-        return NextResponse.json({ error: `Failed to update invoice ${error}` }, { status: 500 });
+        return NextResponse.json({ error: `Failed to update invoice: ${error}` }, { status: 500 });
     }
 }
 
-export async function DELETE( { params }: { params: { id: string } }) {
+
+export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
     try {
-        const deletedFactura = await deleteFactura((params.id));
+        const deletedFactura = await deleteFactura(params.id);
         return NextResponse.json(deletedFactura);
     } catch (error) {
-        return NextResponse.json({ error: `Erro ao tentar deletar o item ${error}` }, { status: 500 });
+        return NextResponse.json({ error: `Error deleting the item: ${error}` }, { status: 500 });
     }
 }
 
-export async function GET( { params }: { params: { id: string } }) {
+
+export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
     try {
-      const factura = await getFacturaById(params.id); 
-  
-      if (!factura) {
-        return NextResponse.json({ error: `Factura ou Cotacao nao encontrada` }, { status: 404 }); 
-      }
-  
-      return NextResponse.json(factura); 
+        const factura = await getFacturaById(params.id); // Acesso correto a 'params.id'
+
+        if (!factura) {
+            return NextResponse.json({ error: `Factura not found` }, { status: 404 });
+        }
+
+        return NextResponse.json(factura);
     } catch (error) {
-      console.error(error);
-      return NextResponse.json({ error: `error ${error}` }, { status: 500 }); 
+        console.error(error);
+        return NextResponse.json({ error: `Error: ${error}` }, { status: 500 });
     }
-  }
+}
