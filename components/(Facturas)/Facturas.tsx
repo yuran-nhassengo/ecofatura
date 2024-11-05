e7e2bb95c03d62c789796d87ea0b7cef52c7f3fe
yuran-nhassengo <yurannhassengo@gmail.com>
2024-11-06 01:26:12 +0200

    add slice

--- components/(Facturas)/Facturas.tsx
+++ components/(Facturas)/Facturas.tsx
@@ -120,29 +120,34 @@ export const Facturas: React.FC = () => {
   };
 
   const sortedFacturas = React.useMemo(() => {
-  const sortableItems = Array.isArray(facturas) ? [...facturas] : [];
+   
+    const sortableItems = Array.isArray(facturas) ? [...facturas] : [];
   
-  if (sortConfig.key) {
-    sortableItems.sort((a: Record<string, any>, b: Record<string, any>) => {
-      if (sortConfig.key === "valor") {
-        return sortConfig.direction === "ascending"
-          ? parseFloat(a[sortConfig.key]) - parseFloat(b[sortConfig.key])
-          : parseFloat(b[sortConfig.key]) - parseFloat(a[sortConfig.key]);
-      } else if (sortConfig.key === "data") {
-        return sortConfig.direction === "ascending"
-          ? new Date(a[sortConfig.key]).getTime() -
-              new Date(b[sortConfig.key]).getTime()
-          : new Date(b[sortConfig.key]).getTime() -
-              new Date(a[sortConfig.key]).getTime();
-      } else {
-        return sortConfig.direction === "ascending"
-          ? a[sortConfig.key].localeCompare(b[sortConfig.key])
-          : b[sortConfig.key].localeCompare(a[sortConfig.key]);
-      }
-    });
-  }
-  return sortableItems;
-}, [facturas, sortConfig]);
+    if (sortConfig.key) {
+     
+      const sorted = sortableItems.slice().sort((a: Record<string, any>, b: Record<string, any>) => {
+        if (sortConfig.key === "valor") {
+          return sortConfig.direction === "ascending"
+            ? parseFloat(a[sortConfig.key]) - parseFloat(b[sortConfig.key])
+            : parseFloat(b[sortConfig.key]) - parseFloat(a[sortConfig.key]);
+        } else if (sortConfig.key === "data") {
+          return sortConfig.direction === "ascending"
+            ? new Date(a[sortConfig.key]).getTime() -
+                new Date(b[sortConfig.key]).getTime()
+            : new Date(b[sortConfig.key]).getTime() -
+                new Date(a[sortConfig.key]).getTime();
+        } else {
+          return sortConfig.direction === "ascending"
+            ? a[sortConfig.key].localeCompare(b[sortConfig.key])
+            : b[sortConfig.key].localeCompare(a[sortConfig.key]);
+        }
+      });
+  
+      return sorted;
+    }
+  
+    return sortableItems;
+  }, [facturas, sortConfig]);
 
   const handleEdit = (index: number) => {
     setForm(facturas[index]);
