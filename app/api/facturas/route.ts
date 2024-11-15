eeae16f302a729bf23cef621f38ce3f638c11918
yurannhassengo <yurannhassengo@gmail.com>
2024-11-15 13:48:11 -0800

    update api

--- app/api/facturas/route.ts
+++ app/api/facturas/route.ts
@@ -22,21 +22,4 @@ export async function POST(request: Request) {
     }
 }
 
-export async function PUT(request: Request, { params }: { params: { id: string } }) {
-    const body = await request.json();
-    try {
-        const updatedFactura = await updateFactura((params.id), body);
-        return NextResponse.json(updatedFactura);
-    } catch (error) {
-        return NextResponse.json({ error: `Failed to update invoice ${error}` }, { status: 500 });
-    }
-}
 
-export async function DELETE( { params }: { params: { id: string } }) {
-    try {
-        const deletedFactura = await deleteFactura((params.id));
-        return NextResponse.json(deletedFactura);
-    } catch (error) {
-        return NextResponse.json({ error: `Failed to delete invoice ${error}`}, { status: 500 });
-    }
-}
